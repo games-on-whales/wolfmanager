a09feaa66f7e6ee57b339d6ac0df25128f428430
salty2011 <salty2011@proton.me>
2025-01-13 21:25:57 +1100

    fix type script error caused by recent changes

--- src/types/config.ts
+++ src/types/config.ts
@@ -11,4 +11,12 @@ export interface SteamGame {
   playtime_forever: number;
   img_icon_url: string;
   img_logo_url: string;
+  rtime_last_played: number;
+  has_community_visible_stats?: boolean;
+  playtime_windows_forever?: number;
+  playtime_mac_forever?: number;
+  playtime_linux_forever?: number;
+  playtime_deck_forever?: number;
+  content_descriptorids?: number[];
+  playtime_disconnected?: number;
 } 
